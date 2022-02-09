f7c51c2043e5e249a3d5092fa0351f8b3cc2c3fa
ChitownCoder <chitowncoder@gmail.com>
2022-02-08 20:52:14 -0600

    Went over managing global state

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 
 function Card({ children, reverse }) {
+  //* this is a conditional class
   //   return <div className={`card ${reverse && "reverse"}`}>{children}</div>;
 
   return (
     <div
       className="card"
+      //* this is a conditional style
       style={{
         backgroundColor: reverse ? "rgba(0,0,0,0.4)" : "#fff",
         color: reverse ? "#fff" : "#000",
